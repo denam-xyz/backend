b3e976a8ee39cdda85308ba1722fc6ab05275cf9
Johanna Fransson <adrina0002@gmail.com>
2022-10-25 18:50:56 +0200

    add logic for ENS lookup/resolver address, no styling added

--- src/pages/DomainSearch.js
+++ src/pages/DomainSearch.js
@@ -2,11 +2,100 @@ import React, { useState, useEffect } from "react";
 import Table from "../components/Table";
 import { useLocation } from "react-router-dom";
 import "../index.css";
+import { ethers } from "ethers";
+import Web3 from "web3";
 
 export default function DomainSearch({ route }) {
   let location = useLocation();
   const { searchInput } = location.state;
+  const [loading, setLoading] = useState();
 
+  const [searchTextInput, setSearchTextInput] = useState();
+  const [hasAddressAssociatedWithENS, setHasAddressAssociatedWithENS] =
+    useState();
+
+  //const [provider, setProvider] = useState();
+
+  const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+  // const network = await web3.eth.net.getNetworkType();
+
+  useEffect(() => {
+    async function fetchData() {
+      setSearchTextInput(searchInput);
+      await window.ethereum.enable();
+      checkIfENSIsOwned(searchInput);
+    }
+    fetchData();
+  }, []);
+
+  const renderLoadingSpinner = () => {
+    return (
+      <div role="status" class="justify-center items-center">
+        <svg
+          aria-hidden="true"
+          class="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"
+          />
+        </svg>
+        <span class="sr-only">Loading...</span>
+      </div>
+    );
+  };
+
+  const checkIfENSIsOwned = async (ensString) => {
+    setLoading(true);
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    let hasAddressAssociatedWithENS = await provider.resolveName(ensString);
+    setHasAddressAssociatedWithENS(hasAddressAssociatedWithENS);
+    setLoading(false);
+  };
+
+  const handleSearchChange = async (e) => {
+    setSearchTextInput(e.target.value);
+    checkIfENSIsOwned(e.target.value);
+  };
+
+  const renderENSLookup = () => {
+    return (
+      <>
+        <div>
+          Does {searchTextInput} have a corresponding owner address?{" "}
+          <div>
+            {" "}
+            {loading ? (
+              renderLoadingSpinner()
+            ) : (
+              <div>
+                {hasAddressAssociatedWithENS
+                  ? hasAddressAssociatedWithENS.toString()
+                  : null}
+                <br></br> <br></br>
+                {hasAddressAssociatedWithENS ? (
+                  <a
+                    target="_blank"
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    href={`https://app.ens.domains/name/${searchTextInput}/details`}
+                  >
+                    LINK TO ADDRESS
+                  </a>
+                ) : null}
+              </div>
+            )}
+          </div>
+        </div>
+      </>
+    );
+  };
   return (
     <div className="align-middle text-center text-banner">
       <div className="h-screen flex ">
@@ -36,8 +125,10 @@ export default function DomainSearch({ route }) {
                   ></path>
                 </svg>
               </div>
+
               <input
-                value={searchInput}
+                value={searchTextInput}
+                onChange={(e) => handleSearchChange(e)}
                 type="search"
                 id="search"
                 class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -52,6 +143,8 @@ export default function DomainSearch({ route }) {
               </button>
             </div>
           </form>
+          {renderENSLookup()}
+
           <Table />
         </div>
       </div>
