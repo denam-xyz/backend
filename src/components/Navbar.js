7fbb2fb95deae948ed32268f3503ab10b2a607e7
thomas779 <57649600+thomas779@users.noreply.github.com>
2022-10-25 09:06:25 +0200

    UI design (#1)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -48,6 +48,14 @@ const Navbar = () => {
               </a>
             </li>
             <li>
+              <a
+                href="#"
+                class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                Docs
+              </a>
+            </li>
+            {/* <li>
               <button
                 id="dropdownNavbarLink"
                 data-dropdown-toggle="dropdownNavbar"
@@ -177,31 +185,7 @@ const Navbar = () => {
                   </a>
                 </div>
               </div>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Services
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Pricing
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Contact
-              </a>
-            </li>
+            </li> */}
           </ul>
         </div>
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 
+import space_id from "../assets/space_id.png";
+import unstoppable_domains from "../assets/unstoppable_domains.png";
+import ens from "../assets/ens.png";
+import near from "../assets/near_logo.png";
+import pns from "../assets/pns.png";
+
 export default function Home() {
   const navigate = useNavigate();
   const [searchInput, setSearchInput] = useState("");
@@ -12,7 +18,7 @@ export default function Home() {
   return (
     <div className="align-middle text-center text-banner -mt-12">
       <div className="h-screen flex ">
-        <div className="grow space-y-10 self-center">
+        <div className="grow space-y-12 self-center">
           <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
             Discover your web3 identity
           </h1>
@@ -53,69 +59,78 @@ export default function Home() {
                 }}
                 type="search"
                 id="search"
-                class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="block p-5 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Find your new domain"
                 required
               />
               <button
                 onClick={() => handleSearchClick()}
                 type="submit"
-                class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Search
               </button>
             </div>
           </form>
-          <h4 class="text-2xl font-bold dark:text-white pt-20">
+          <h4 class="text-2xl font-bold dark:text-white pt-20 pb-8">
             Search for decentralized names with our verified integrations
           </h4>
+          <div class="flex justify-evenly relative">
+            <img class="h-12" src={pns} alt="image description" />
+            <img
+              class="h-11"
+              src={unstoppable_domains}
+              alt="image description"
+            />
+            <img class="h-7" src={space_id} alt="image description" />
+            <img class="h-11 rounded-lg" src={ens} alt="image description" />
+            <img class="h-10" src={near} alt="image description" />
+          </div>
         </div>
       </div>
-      <div className="flex bg-gray-900 text-white h-screen">
-        <div className="grow space-y-20 self-center">
+      <div className="flex bg-gray-900 text-white h-screen pt-20 pb-2">
+        <div className="grow self-center">
           <h1 class="text-5xl font-extrabold dark:text-gray-900">
             Stress-free names, powered by denam
           </h1>
-          <h3 class="text-3xl font-extrabold ml-2 font-semibold text-gray-500 dark:text-gray-400">
-            Put the power of denam behind your decentralized identity.
-          </h3>
-          <p></p>
-          <div class="grid grid-cols-3 gap-y-4 gap-x-1">
-            <div className="">
-              <h3 class="text-3xl font-bold dark:text-white">Simple Search</h3>
+          <p class="mb-6 mt-6 text-lg font-extrabold  font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+            Put the power of denam behind your decentralized identity
+          </p>
+          <div class="flex justify-evenly m-20">
+            <div className="p-8">
+              <h3 class="text-3xl font-bold dark:text-white mb-2">
+                Simplified Search
+              </h3>
               <p class="mb-3 text-lg font-light text-gray-400 md:text-xl dark:text-gray-400">
-                A Web3 domain uniquely represents your on-chain indentity. We
+                A Web3 domain uniquely represents your on-chain identity. We
                 make it easy.
               </p>
             </div>
-            <div class="">
-              <h3 class="text-3xl font-bold dark:text-white">
-                Multichain-Enabled
+            <div class="p-8">
+              <h3 class="text-3xl font-bold dark:text-white mb-2">
+                Multichain Enabled
               </h3>
               <p class="mb-3 text-lg font-light text-gray-400 md:text-xl dark:text-gray-400">
-                Whether that be Ethereum, Polygon, or Avalanche. We got your
+                Whether that be Ethereum, Aptos, or Avalanche. We got your
                 covered.
               </p>
             </div>
-            <div class="">
-              <h3 class="text-3xl font-bold dark:text-white">
-                Empowering the Ecosystem
+            <div class="p-8">
+              <h3 class="text-3xl font-bold dark:text-white mb-2">
+                Ecosystem Empowering
               </h3>
               <p class="mb-3 text-lg font-light text-gray-400 md:text-xl dark:text-gray-400">
                 With 100+ of blockchains, join the ever-growing domain
                 ecosystem.
               </p>
             </div>
-            <div class="col-start-1 col-end-7 mt-14">
-              <h3 class="text-3xl font-bold dark:text-white">
-                Searching for your new domain shouldn't incur additional
-                charges.<br className="mt-5"></br>
-                <span class="underline underline-offset-3 decoration-4 decoration-blue-400 dark:decoration-blue-600">
-                  We believe in empowering users with their web3 identitiy.
-                </span>
-              </h3>
-            </div>
           </div>
+          <h3 class="text-3xl font-bold dark:text-white">
+            Connecting your{" "}
+            <mark class="px-1 text-gray-900 bg-white rounded dark:bg-gray-900 dark:text-white">
+              web3 name
+            </mark>
+          </h3>
         </div>
       </div>
       {/* <div className="h-screen">
