94fa020dceb05b812631c1aef6124c60e83b750c
thomas779 <thomasehumphreys@gmail.com>
2022-11-23 19:45:08 +0100

    testing cors

--- v1/index.js
+++ v1/index.js
@@ -8,6 +8,25 @@ app.use(bodyParser());
 app.use(bodyParser.json());
 app.use(cors());
 
+var corsOptions = {
+  origin: "https://denam.on.fleek.co",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.get(
+  "/v1/unstoppableDomains/search/:search",
+  cors(corsOptions),
+  function (req, res, next) {
+    res.json({
+      msg: "This is CORS-enabled for only https://denam.on.fleek.co.",
+    });
+  }
+);
+
+app.listen(80, function () {
+  console.log("CORS-enabled web server listening on port 80");
+});
+
 for (var key in endpoints) {
   var endpoint = endpoints[key];
 
